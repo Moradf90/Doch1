9b73c17a4e26f6f1585bea7799ee400c023099bf
tomdinur <tomdinur@gmail.com>
2016-11-27 23:02:20 +0200

    שמע מקווה שהפרויקט לא יהרס אחרי הקומיט הזה

--- Code/app/src/main/java/t/a/m/com/doch1/LoginActivity.java
+++ Code/app/src/main/java/t/a/m/com/doch1/LoginActivity.java
@@ -1,13 +1,108 @@
 package t.a.m.com.doch1;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LoginActivity extends Activity {
+
+    private EditText emailEditText;
+    private EditText passEditText;
+    private Button btnSignIn;
+    private TextView btnForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Address the email and password field
+        emailEditText = (EditText) findViewById(R.id.txt_username);
+        passEditText = (EditText) findViewById(R.id.txt_password);
+        btnSignIn = (Button) findViewById(R.id.btn_sign_in);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkLogin(view);
+            }
+        });
+
+        btnForgotPassword = (TextView) findViewById(R.id.btn_forgot_password);
+
+        btnForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                forgotPassword();
+            }
+        });
+    }
+
+    public void checkLogin(View arg0) {
+
+        final String email = emailEditText.getText().toString();
+        if (!isValidEmail(email)) {
+            //Set error message for email field
+            emailEditText.setError("Invalid Email");
+        }
+
+        final String pass = passEditText.getText().toString();
+        if (!isValidPassword(pass)) {
+            //Set error message for password field
+            passEditText.setError("Password must be more than 4 chars");
+        }
+
+        else if(isValidEmail(email) && isValidPassword(pass))
+        {
+            // TODO: get role from server if user exist, save on phone the role and name for next uses?
+            if ((email.equals("tomdinur@gmail.com")) && pass.equals("12345")) {
+                Toast.makeText(LoginActivity.this, "Morad's turn", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    // validating email id
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // validating password
+    private boolean isValidPassword(String pass) {
+        if (pass != null && pass.length() >= 4) {
+            return true;
+        }
+        return false;
+    }
+
+    private void forgotPassword() {
+        final String email = emailEditText.getText().toString();
+        if (isValidEmail(email)) {
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("message/rfc822");
+            i.putExtra(Intent.EXTRA_EMAIL  , new String[]{email});
+            i.putExtra(Intent.EXTRA_SUBJECT, "Password reset - Doch1 ");
+            i.putExtra(Intent.EXTRA_TEXT, "Your password is... 12345");
+            try {
+                startActivity(Intent.createChooser(i, "Send mail..."));
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(LoginActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
